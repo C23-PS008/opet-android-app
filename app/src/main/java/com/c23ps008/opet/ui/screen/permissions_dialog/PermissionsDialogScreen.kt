52212835ee1334159183dec9ea6b0700bd516273
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-06-15 18:02:35 +0700

    fix permission message

--- app/src/main/java/com/c23ps008/opet/ui/screen/permissions_dialog/PermissionsDialogScreen.kt
+++ app/src/main/java/com/c23ps008/opet/ui/screen/permissions_dialog/PermissionsDialogScreen.kt
@@ -68,9 +68,9 @@ class CameraPermissionTextProvider : PermissionTextProvider {
 class LocationPermissionTextProvider : PermissionTextProvider {
     override fun getDescription(isPermanentlyDeclined: Boolean): String {
         return if (isPermanentlyDeclined) {
-            "The camera permission has been permanently declined. To use the camera feature, the camera permission needs to be granted. Please open the app settings and enable the camera permission to continue using this feature."
+            "The location permission has been permanently declined. To use the location feature, the location permission needs to be granted. Please open the app settings and enable the location permission to continue using this feature."
         } else {
-            "To access the camera, the app requires permission to use the camera. Please grant the camera permission to enable the camera functionality."
+            "To access the location, the app requires permission to use the location. Please grant the location permission to enable the location functionality."
         }
     }
 }
